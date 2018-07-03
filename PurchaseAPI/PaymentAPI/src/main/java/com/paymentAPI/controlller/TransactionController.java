8cd5d917e329e9c968c36465cff4baffcc04a91f
Nguyen Vu Long <nguyenvulong2001@gmail.com>
2018-07-03 15:03:31 +0700

    Add method post transaction

--- PurchaseAPI/PaymentAPI/src/main/java/com/paymentAPI/controlller/TransactionController.java
+++ PurchaseAPI/PaymentAPI/src/main/java/com/paymentAPI/controlller/TransactionController.java
@@ -3,10 +3,14 @@ package com.paymentAPI.controlller;
 import java.sql.Timestamp;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -53,9 +57,18 @@ public class TransactionController {
 	
 	@RequestMapping(value = "/add", //
 			method = RequestMethod.POST)
-	public int addTransaction() {
-		
-		int i = trxService.addTrx(null, null, null, null, null);
+	public int addTransaction(HttpServletRequest request, HttpServletResponse response,
+			Model model) {
+		String seriNo  = request.getParameter("seri_no");
+		String trxNum = request.getParameter("trx_number");
+		Timestamp trxDate = new Timestamp(System.currentTimeMillis());
+		Long trxTime  = 0l;
+		Long userId = Long.parseLong(request.getParameter("user_id"));
+		System.out.println("Data is seriNo: "+ seriNo + "\n trxNum: " + trxNum + 
+				"\n trxDate " + trxDate +
+				"\n trxTime " + trxTime +
+				"\n userId" + userId); 
+		int i = trxService.addTrx(seriNo, trxNum, trxDate, trxTime, userId);
 		return i;
 	}
 }
